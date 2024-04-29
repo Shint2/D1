fdabdc70a62a91b72872ffbfbcf5616b820935d7
Shint <reecewilliams2009@hotmail.co.uk>
2024-04-30 00:31:30 +0100

    Add ping and set time functions. added more buttons

---
+++ secret.py
@@ -0,0 +1,22 @@
+# WiFi
+ssid = ""
+password = ""
+
+# OpenWeather API
+openweather_api_key = ""
+city = ""
+lang = ""
+units = ""
+
+# MQTT
+mqttusername = ""
+mqttpassword = ""
+mqtt_broker = ""
+client_id = ""
+topic_msg = b""
+
+#MQTT switches
+switch1 = b""
+switch2 = b""
+switch3 = b""
+switch4 = b""
